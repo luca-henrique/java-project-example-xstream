fa3fdab384439442b1cf6095de9d2a27ceb465ee
Lucas Henrique <lucashenrique@Mac-Pro-de-Lucas.local>
2021-11-10 11:50:11 -0300

    first commit

---
+++ src/components/Button.java
@@ -0,0 +1,13 @@
+package components;
+
+import javax.swing.JButton;
+
+public class Button extends JButton {
+
+	private static final long serialVersionUID = 1L;
+
+	public Button(String nome) {
+
+	}
+
+}
